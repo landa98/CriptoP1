8239d9c136cdda237fca97aa875c80fd21ab4971
Ulises Landázuri <u.landazuri.98@gmail.com>
2021-10-11 18:38:43 -0500

    [ADD] Propuesta para clase IO

--- App/services/io.py
+++ App/services/io.py
@@ -1,7 +1,47 @@
 class IO(object):
-    def __init__(self):
+
+    @classmethod
+    def abrir_archivo(ruta):
+        '''
+        Wrapper para apertura de archivo
+        '''
         ruta = input("Introduzca nombre del archivo: ")
         try:
             archivo = open(ruta)
         except IOError:
-            print(f"[EROR] No se ha encontrado el archivo {ruta}."
+            print(f"[ERROR] No se ha encontrado el archivo {ruta}.")
+        else:
+            return archivo
+    
+    @classmethod
+    def archivo_a_cadena(archivo):
+        '''
+        Extrae el contenido de un objeto File
+        '''
+        try:
+            cadena = ""
+            with archivo as f:
+                lineas = f.readlines()
+            for linea in lineas:
+                cadena += linea.rstrip()
+        except IOError:
+            print(f"[ERROR] No se pudo leer el archivo {archivo.name}.")
+        else:
+            return cadena
+
+
+    @classmethod
+    def guardar_cadena(ruta, cadena):
+        '''
+        Wrapper para escritura de archivo
+        '''
+        try:
+            with open(ruta, 'w') as f:
+                f.write(cadena)
+        except IOError:
+            print(f"[ERROR] El archivo {ruta} se encuentra abierto o no se puede escribir.")
+        else:
+            return True
+
+        
+
