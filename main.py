ee205a74a510a308921fdd320928b4d755a8837d
Ulises Landázuri <u.landazuri.98@gmail.com>
2021-10-11 18:06:19 -0500

    [MOD] Propuesta inicial para main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from App.components.afin import Afin
 from App.components.columnas import Columnas
 from App.components.vernam import Vernam
+from App.services.leer import Leer
+from App.services.escribir import Escribir
+
+def main():
+    archivo = Leer()
+
+if __name__ == "__main__":
+    main()
